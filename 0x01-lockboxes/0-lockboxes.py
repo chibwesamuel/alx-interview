6e2fdff3207ae3969fb7b1b1d6b470531005f404
Samuel Chibwe <smchibwe@gmail.com>
2023-07-05 16:00:28 +0000

    New fixes to file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
@@ -11,15 +12,14 @@ def canUnlockAll(boxes):
         bool: True if all boxes can be opened, otherwise False.
     """
 
-    n = len(boxes) # Declares Total number of boxes
-    unlocked = {0} # Set to track all unlocked boxes
-    keys = set(boxes[0]) # Set to store keys in the first box
+    n = len(boxes)  # Declares Total number of boxes
+    unlocked = {0}  # Set to track all unlocked boxes
+    keys = set(boxes[0])  # Set to store keys in the first box
 
     while keys:
         key = keys.pop()
         if key < n and key not in unlocked:
             unlocked.add(key)
             keys.update(boxes[key])
-
 
     return len(unlocked) == n
