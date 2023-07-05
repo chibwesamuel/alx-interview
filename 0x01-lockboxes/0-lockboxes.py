e1d49c464af8e577b5caa5c4c7a84313e55b9beb
Samuel Chibwe <smchibwe@gmail.com>
2023-07-05 16:11:07 +0000

    Changed from while to for loop

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,20 +6,18 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened.
 
     Args:
-    boxes (list): A list of lists representing the boxes and their corresponding keys.
+        boxes (list): A list of lists representing the boxes and their corresponding keys.
 
     Returns:
         bool: True if all boxes can be opened, otherwise False.
     """
 
-    n = len(boxes)  # Declares Total number of boxes
-    unlocked = {0}  # Set to track all unlocked boxes
-    keys = set(boxes[0])  # Set to store keys in the first box
+    n = len(boxes)  # Total number of boxes
+    unlocked = set()  # Set to track the unlocked boxes
+    keys = set(boxes[0])  # Set to store the keys in the first box
 
-    while keys:
-        key = keys.pop()
-        if key < n and key not in unlocked:
-            unlocked.add(key)
-            keys.update(boxes[key])
+    for box in range(n):
+        if box in unlocked or box == 0:
+            unlocked.update(boxes[box])
 
     return len(unlocked) == n
