af1023569286d95c91c70e95b055f90d09942260
Samuel Chibwe <smchibwe@gmail.com>
2023-08-20 13:44:51 +0000

    Documentation fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,7 +7,7 @@ Returns: None. The matrix is edited in-place.
 """
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix):
     """
     Rotates a given n x n 2D matrix 90 degrees clockwise in-place.
 
@@ -30,13 +30,37 @@ def rotate_2d_matrix(matrix: List[List[int]]) -> None:
 
 
 if __name__ == "__main__":
-    """ Test the rotate_2d_matrix function"""
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-    
-    rotate_2d_matrix(matrix)
-    
+    """
+    Test the rotate_2d_matrix function with different matrices.
+    """
+
+    # Test Case 1
+    matrix1 = [[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]]
+    rotate_2d_matrix(matrix1)
     print("Rotated Matrix:")
-    for row in matrix:
+    for row in matrix1:
         print(row)
+
+    # Test Case 2
+    matrix2 = [[1, 11, 13, 19],
+               [41, 32, 6, 22],
+               [55, 21, 41, 66],
+               [77, 29, 78, 2]]
+    rotate_2d_matrix(matrix2)
+    print("Rotated Matrix:")
+    for row in matrix2:
+        print(row)
+
+    # Test Case 3
+    matrix3 = [[1, 2, 3, 4, 5],
+               [6, 7, 8, 9, 10],
+               [11, 12, 13, 14, 15],
+               [16, 17, 18, 19, 20],
+               [21, 22, 23, 24, 25]]
+    rotate_2d_matrix(matrix3)
+    print("Rotated Matrix:")
+    for row in matrix3:
+        print(row)
+
