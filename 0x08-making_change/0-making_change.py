9e2d64189800b854c903eb5009f3800cf40b9f30
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 08:22:33 +0200

    Code now working

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,7 +17,8 @@ def makeChange(coins, total):
         return 0
 
     # Initialize a list to store the minimum number of coins needed for each
-    value from 0 to total min_coins = [float('inf')] * (total + 1)
+    # value from 0 to total
+    min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0  # Base case: 0 coins needed to make a total of 0
 
     # Iterate through each coin value
@@ -25,15 +26,13 @@ def makeChange(coins, total):
         # Iterate through each total value
         for value in range(coin, total + 1):
             # Update the minimum number of coins needed for the current total
-            min_coins[value] = min(min_coins[value], min_coins[value - coin]
-            + 1)
+            min_coins[value] = min(min_coins[value], min_coins[value - coin] + 1)
 
-    # If the minimum number of coins for the target total is still infinity,
-    it means it's not possible
-    # return min_coins[total] if min_coins[total] != float('inf') else -1
+    # If the minimum number of coins for the target total is still infinity
+    # it means it's not possible
+    return min_coins[total] if min_coins[total] != float('inf') else -1
 
 
 if __name__ == "__main__":
     print(makeChange([1, 2, 25], 37))
     print(makeChange([1256, 54, 48, 16, 102], 1453))
-
